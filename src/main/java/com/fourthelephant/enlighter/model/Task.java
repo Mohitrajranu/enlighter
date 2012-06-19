ba4fc2bc6ce6cddf57edcfbad9c058dc16ab8a06
Vadim Bobrov <vadimbobrov@gmail.com>
2012-06-19 13:13:11 -0400

    works on both JBoss and Tomcat

--- src/main/java/com/fourthelephant/enlighter/model/Task.java
+++ src/main/java/com/fourthelephant/enlighter/model/Task.java
@@ -1,13 +1,9 @@
 package com.fourthelephant.enlighter.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Size;
-
 import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Task {
